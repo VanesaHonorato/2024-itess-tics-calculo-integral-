ace45bccdf2c6a70cfc1f63a728a63cf33067fbb
VanesaHonorato <vanegoho@gmail.com>
2024-02-14 11:43:10 -0600

    carga inicial

---
+++ src/FunctAproximation/Test.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FunctAproximation;
+
+/**
+ *
+ * @author vanesahonorato
+ */
+public class Test {
+    public static void main(String[]args){
+        FunctAproximation fa = new FunctionAproximation (0.0, 4.0, 4);
+        fa.cumpute();
+        System.out.println("area:"+ fa.getArea());
+  
+           
+    }
+    
+}
